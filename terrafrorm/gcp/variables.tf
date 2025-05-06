ffb09214572a7efa53fcb34461ee5df251c94582
Wojtek Wyleżoł <w.wylezol@wp.pl>
2025-05-06 21:14:10 +0200

    load test - test optimalization

---
+++ terrafrorm/gcp/variables.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  description = "atlantean-yeti-454021-b3"
+  type        = string
+}
+
+variable "region" {
+  description = "Region Google Cloud"
+  type        = string
+  default     = "europe-central2"
+}
